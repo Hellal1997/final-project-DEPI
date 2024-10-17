a52e5d16de461e5a1eb6546241ba0cfd1a1863f3
Mohamed Helal 🇵🇸 <153162650+Hellal1997@users.noreply.github.com>
2024-10-18 01:04:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,47 +1,94 @@
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #!/bin/bash
-#Install Java
-echo "--------------------Installing jenkins--------------------"
-sudo apt update -y
-
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-
-
- echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
-    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-    /etc/apt/sources.list.d/jenkins.list > /dev/null
-
-    sudo apt-get update
-  sudo apt-get install fontconfig openjdk-17-jre
-  sudo apt-get install jenkins
-
-
-java -version
-sudo systemctl status jenkins
-
-#Install docker
-echo "--------------------Installing Docker--------------------"
-sudo apt-get update 
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
+ansible :
+sudo apt update
+sudo apt install -y software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+aws:
+#install aws cli
+sudo apt update
+sudo apt install awscli
+sudo apt install curl unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+aws configure
+    #then add access key and secret access key
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+docker:
+#Ubuntu
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-#Install ansible
-echo "--------------------Installing Ansible--------------------"
-sudo apt update -y
-sudo apt install software-properties-common -y
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible -y
-#Add docker to sudo group
-echo "--------------------Add Docker to Sudo group--------------------"
-sudo groupadd docker && sudo usermod -aG docker $USER && sudo chmod 777 /var/run/docker.sock
-#Show Jenkins Password
-echo "--------------------Jenkins Password--------------------"
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+#Centos
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+gcloud:
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg curl
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update && sudo apt-get install google-cloud-cli
+# IAM & Admin > Service Accounts > Create Service Account > Assign Roles > In the Service Accounts > three dots on the right and choose Manage keys > dd Key > Create New Key > Select JSON
+gcloud auth activate-service-account [JSON_SERVICE_ACCOUNT_EMAIL] --key-file=[JSON_KEY_FILE]
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+jenkins:
+docker run -p 8080:8080 -p 50000:50000 -d \                                                                                                                                               ✔ 
+-v jenkins_home:/var/jenkins_home \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v $(which docker):/usr/bin/docker jenkins/jenkins:jdk21
+
+
+#then
+#on host  : 
+  getent group docker -> docker:x:999:your_username
+  sudo chmod 660 /var/run/docker.sock
+
+docker exec -u root -it jenkins /bin/bash 
+  groupadd -g 999 docker
+  usermod -aG docker jenkins
+  chown -R jenkins:jenkins /var/jenkins_home
+  chmod -R 775 /var/jenkins_home
+
+docker restart jenkins
+
+#install python :
+apt-get update
+apt-get install -y python3 python3-pip
+apt-get install -y python3-venv
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+kubernates:
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+terraform:
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+
+sudo apt-get install terraform
+
+terraform -version
+
+
+#infracost
+curl -fsSL https://raw.githubusercontent.com/infracost/infracost/master/scripts/install.sh | sh
+cd my-terraform-project
+infracost breakdown --path .
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
