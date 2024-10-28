96a2e93119175538716ef5516d364589a8cc3b3b
Mohamed Helal 🇵🇸 <153162650+Hellal1997@users.noreply.github.com>
2024-10-28 12:14:05 +0300

    Update script.js

--- commingsoon/script.js
+++ commingsoon/script.js
@@ -1,170 +1,396 @@
-var current_page = 1
+// TMDB Movie API
+const API_KEY = "api_key=ec332d19e6fed067df0160ce34067cc4"; // API Key
+const BASE_URL = "https://api.themoviedb.org/3";
+// const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`; // Template literal
+const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+const SEARCH_URL = `${BASE_URL}/search/movie?${API_KEY}`;
 
-function load_suras() {
-  p = $.ajax({
-    url: 'json/suras.json',
-    dataType: 'json'
-  })
+const genres = [
+	{
+		id: 28,
+		name: "Action",
+	},
+	{
+		id: 12,
+		name: "Adventure",
+	},
+	{
+		id: 16,
+		name: "Animation",
+	},
+	{
+		id: 35,
+		name: "Comedy",
+	},
+	{
+		id: 80,
+		name: "Crime",
+	},
+	{
+		id: 99,
+		name: "Documentary",
+	},
+	{
+		id: 18,
+		name: "Drama",
+	},
+	{
+		id: 10751,
+		name: "Family",
+	},
+	{
+		id: 14,
+		name: "Fantasy",
+	},
+	{
+		id: 36,
+		name: "History",
+	},
+	{
+		id: 27,
+		name: "Horror",
+	},
+	{
+		id: 10402,
+		name: "Music",
+	},
+	{
+		id: 9648,
+		name: "Mystery",
+	},
+	{
+		id: 10749,
+		name: "Romance",
+	},
+	{
+		id: 878,
+		name: "Science Fiction",
+	},
+	{
+		id: 10770,
+		name: "TV Movie",
+	},
+	{
+		id: 53,
+		name: "Thriller",
+	},
+	{
+		id: 10752,
+		name: "War",
+	},
+	{
+		id: 37,
+		name: "Western",
+	},
+];
 
-  p.done(function (data) {
-    str = ''
-    for (var i = 0; i < data.length; i++) {
-      sura = data[i]
-      str += '<tr id="sura_link_' + sura.id + '">'
-      str += '<td>' + (i + 1) + '</td>'
-      str += '<td> <a class="sura_link" href="" '
-      str += 'data-page="' + sura.page + '" >'
-      str += sura.name + '</a></td>'
-      str += '<td>' + sura.page + '</td>'
-      str += '<td>' + sura.ayas + '</td>'
-      str += '</tr>'
-    }
-    $('#suras tbody').html(str)
-  })
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const tagsEl = document.getElementById("tags");
+
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const current = document.getElementById("current");
+
+let currentPage = 1;
+let nextPage = 2;
+let prevPage = 3;
+let lastUrl = "";
+let totalPages = 100;
+
+window.addEventListener("load", function () {
+	let loader = document.querySelector(".loader");
+	loader.style.display = "none"; // Hide the loader when the window finishes loading
+});
+
+let selectedGenre = [];
+setGenre();
+function setGenre() {
+	tagsEl.innerHTML = "";
+	genres.forEach((genre) => {
+		const t = document.createElement("div");
+		t.classList.add("tag");
+		t.id = genre.id;
+		t.innerText = genre.name;
+		t.addEventListener("click", () => {
+			if (selectedGenre.length == 0) {
+				selectedGenre.push(genre.id);
+			} else {
+				if (selectedGenre.includes(genre.id)) {
+					selectedGenre.forEach((id, idx) => {
+						if (id == genre.id) {
+							selectedGenre.splice(idx, 1);
+						}
+					});
+				} else {
+					selectedGenre.push(genre.id);
+				}
+			}
+			console.log(selectedGenre);
+			getMovies(API_URL + "&with_genres=" + encodeURI(selectedGenre.join(",")));
+			highlightSelection();
+		});
+		tagsEl.append(t);
+	});
 }
 
-function sura_clicked(event) {
-  event.preventDefault()
-  event.stopPropagation()
-  el = event.target
-  page = $(el).data('page')
-  // console.log('Sura Clicked!' + page);
-  load_page(page)
+function highlightSelection() {
+	const tags = document.querySelectorAll(".tag");
+	tags.forEach((tag) => {
+		tag.classList.remove("highlight");
+	});
+	clearBtn();
+	if (selectedGenre.length != 0) {
+		selectedGenre.forEach((id) => {
+			const hightlightedTag = document.getElementById(id);
+			hightlightedTag.classList.add("highlight");
+		});
+	}
 }
 
-function load_page(page) {
-  if (page < 1) page = 1
-  if (page > 604) page = 604
-  current_page = page
-  $('.control__page-num').html('صفحة : ' + current_page)
-
-  $page = $('#page')
-  $page.html('')
-  $taf = $('#tafseer')
-  $taf.html('')
-
-  if (page < 10) {
-    page_str = '00' + page
-  } else if (page < 100) {
-    page_str = '0' + page
-  } else {
-    page_str = '' + page
-  }
-  $page.css('background-image', 'url(img/' + page_str + '.jpg)')
-
-  // aya segments
-  p = $.ajax({
-    url: 'json/page_' + page + '.json',
-    dataType: 'json'
-  })
-
-  p.fail(function (data) {
-    console.log('Failed to load page map!')
-  })
-
-  p.done(function (data) {
-    // Clear selected
-    $('#suras tr').removeClass('active')
-    for (var i = 0; i < data.length; i++) {
-      aya = data[i]
-      // console.log('Sura:' + aya.sura_id+' Aya:'+aya.aya_id);
-      // Activate Sura
-      $('#sura_link_' + aya.sura_id).addClass('active')
-
-      $a = $('<a>')
-      $a.attr('href', '#' + aya.aya_id)
-      $a.data('sura', aya.sura_id)
-      $a.data('aya', aya.aya_id)
-      $a.addClass('aya_link')
-      for (var j = 0; j < aya.segs.length; j++) {
-        seg = aya.segs[j]
-        if (seg.w != 0 && seg.w < 15) continue
-        if (seg.x < 15) {
-          seg.w += seg.x
-          seg.x = 0
-        }
-        $d = $('<div>')
-          .css('top', seg.y + 'px')
-          .css('left', seg.x + 'px')
-          .css('width', seg.w + 'px')
-          .css('height', seg.h + 'px')
-        $a.append($d)
-        // console.log('Segment:'+aya.sura_id+' Aya '+aya.aya_id);
-      }
-      $page.append($a)
-    }
-  })
+function clearBtn() {
+	let clearBtn = document.getElementById("clear");
+	if (clearBtn) {
+		clearBtn.classList.add("highlight");
+	} else {
+		let clear = document.createElement("div");
+		clear.classList.add("tag", "highlight");
+		clear.id = "clear";
+		clear.innerText = "Clear x";
+		clear.addEventListener("click", () => {
+			selectedGenre = [];
+			setGenre();
+			getMovies(API_URL);
+		});
+		tagsEl.append(clear);
+	}
 }
 
-function aya_clicked(event) {
-  event.preventDefault()
-  event.stopPropagation()
-  el = $(event.target).closest('a')
-  sura = el.data('sura')
-  aya = el.data('aya')
-  $('a.aya_link').removeClass('active')
-  el.addClass('active')
-  // console.log('Aya Clicked!' + sura + ' ' + aya);
-  load_aya(sura, aya)
+getMovies(API_URL);
+
+function getMovies(url) {
+	lastUrl = url;
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data.results);
+			if (data.results.length !== 0) {
+				showMovies(data.results);
+				currentPage = data.page;
+				nextPage = currentPage + 1;
+				prevPage = currentPage - 1;
+				totalPages = data.total_pages;
+
+				current.innerText = currentPage;
+
+				if (currentPage <= 1) {
+					prev.classList.add("disabled");
+					next.classList.remove("disabled");
+				} else if (currentPage >= totalPages) {
+					prev.classList.remove("disabled");
+					next.classList.add("disabled");
+				} else {
+					prev.classList.remove("disabled");
+					next.classList.remove("disabled");
+				}
+
+				tagsEl.scrollIntoView({ behavior: "smooth" });
+			} else {
+				main.innerHTML = `<h1 class="no-results">No Results Found</h1>`;
+			}
+		});
 }
 
-function load_aya(sura, aya) {
-  var tafseer_name = Array('مشكل', 'نصي', 'الجلالين', 'الميسر', 'ابن كثير')
-  $taf = $('#tafseer')
-  $taf.html('')
+function showMovies(data) {
+	main.innerHTML = "";
 
-  p = $.ajax({
-    url: 'json/aya_' + sura + '_' + aya + '.json',
-    dataType: 'json'
-  })
+	data.forEach((movie) => {
+		const { title, poster_path, vote_average, overview, id } = movie;
+		const movieEl = document.createElement("div");
+		movieEl.classList.add("movie");
+		movieEl.innerHTML = `
+             <img src="${
+								poster_path
+									? IMAGE_URL + poster_path
+									: "http://via.placeholder.com/1080x1580"
+							}" alt="${title}">
 
-  p.fail(function (data) {
-    console.log('Failed to load Tafseer!')
-  })
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
 
-  p.done(function (data) {
-    str = ''
-    for (var i = 0; i < data.length; i++) {
-      taf = data[i]
-      str +=
-        '<strong>' +
-        tafseer_name[taf.type] +
-        '</strong><br>' +
-        taf.text +
-        '<hr>'
-    }
-    $taf.html(str)
-  })
+            <div class="overview">
+
+                <h3>Overview</h3>
+                ${overview}
+                <br/> 
+                <button class="know-more" id="${id}">Know More</button
+            </div>
+        
+        `;
+
+		main.appendChild(movieEl);
+
+		document.getElementById(id).addEventListener("click", () => {
+			console.log(id);
+			openNav(movie);
+		});
+	});
 }
 
-function page_change(event) {
-  event.preventDefault()
-  event.stopPropagation()
-  el = $(event.target)
-  offset = el.data('offset')
-  console.log('Offset:' + offset)
-  page = parseInt(current_page) + offset
-  load_page(page)
+const overlayContent = document.getElementById("overlay-content");
+/* Open when someone clicks on the span element */
+function openNav(movie) {
+	let id = movie.id;
+	fetch(BASE_URL + "/movie/" + id + "/videos?" + API_KEY)
+		.then((res) => res.json())
+		.then((videoData) => {
+			console.log(videoData);
+			if (videoData) {
+				document.getElementById("myNav").style.width = "100%";
+				if (videoData.results.length > 0) {
+					let embed = [];
+					let dots = [];
+					videoData.results.forEach((video, idx) => {
+						let { name, key, site } = video;
+
+						if (site == "YouTube") {
+							embed.push(`
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          
+          `);
+
+							dots.push(`
+              <span class="dot">${idx + 1}</span>
+            `);
+						}
+					});
+
+					let content = `
+        <h1 class="no-results">${movie.original_title}</h1>
+        <br/>
+        
+        ${embed.join("")}
+        <br/>
+
+        <div class="dots">${dots.join("")}</div>
+        
+        `;
+					overlayContent.innerHTML = content;
+					activeSlide = 0;
+					showVideos();
+				} else {
+					overlayContent.innerHTML = `<h1 class="no-results">No Results Found</h1>`;
+				}
+			}
+		});
 }
 
-$(function () {
-  console.log('JQuery Started!')
-  load_suras()
-  load_page(1)
-  $(document).on('click', 'a.sura_link', sura_clicked)
-  $(document).on('click', 'a.aya_link', aya_clicked)
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+	document.getElementById("myNav").style.width = "0%";
+}
 
-  $('.control__button').click(page_change)
+let activeSlide = 0;
+let totalVideos = 0;
 
-  // Hotkeys
-  //$(document).bind('keydown', 'right', function() { p = parseInt(current_page) -1; document.location='#?page='+ p; }  );
-  //$(document).bind('keydown', 'left', function() { p = parseInt(current_page) +1; document.location='#?page='+ p; }  );
-  $(document).bind('keydown', 'right', function () {
-    p = parseInt(current_page) - 1
-    load_page(p)
-  })
-  $(document).bind('keydown', 'left', function () {
-    p = parseInt(current_page) + 1
-    load_page(p)
-  })
-})
+function showVideos() {
+	let embedClasses = document.querySelectorAll(".embed");
+	let dots = document.querySelectorAll(".dot");
+
+	totalVideos = embedClasses.length;
+	embedClasses.forEach((embedTag, idx) => {
+		if (activeSlide == idx) {
+			embedTag.classList.add("show");
+			embedTag.classList.remove("hide");
+		} else {
+			embedTag.classList.add("hide");
+			embedTag.classList.remove("show");
+		}
+	});
+
+	dots.forEach((dot, indx) => {
+		if (activeSlide == indx) {
+			dot.classList.add("active");
+		} else {
+			dot.classList.remove("active");
+		}
+	});
+}
+
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+
+leftArrow.addEventListener("click", () => {
+	if (activeSlide > 0) {
+		activeSlide--;
+	} else {
+		activeSlide = totalVideos - 1;
+	}
+
+	showVideos();
+});
+
+rightArrow.addEventListener("click", () => {
+	if (activeSlide < totalVideos - 1) {
+		activeSlide++;
+	} else {
+		activeSlide = 0;
+	}
+	showVideos();
+});
+
+function getColor(vote) {
+	if (vote >= 8) {
+		return "green";
+	} else if (vote >= 5) {
+		return "orange";
+	} else {
+		return "red";
+	}
+}
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	const searchTerm = search.value;
+	selectedGenre = [];
+	setGenre();
+	if (searchTerm) {
+		getMovies(SEARCH_URL + "&query=" + searchTerm);
+	} else {
+		getMovies(API_URL);
+	}
+});
+
+prev.addEventListener("click", () => {
+	if (prevPage > 0) {
+		pageCall(prevPage);
+	}
+});
+
+next.addEventListener("click", () => {
+	if (nextPage <= totalPages) {
+		pageCall(nextPage);
+	}
+});
+
+function pageCall(page) {
+	let urlSplit = lastUrl.split("?");
+	let queryParams = urlSplit[1].split("&");
+	let key = queryParams[queryParams.length - 1].split("=");
+	if (key[0] != "page") {
+		let url = lastUrl + "&page=" + page;
+		getMovies(url);
+	} else {
+		key[1] = page.toString();
+		let a = key.join("=");
+		queryParams[queryParams.length - 1] = a;
+		let b = queryParams.join("&");
+		let url = urlSplit[0] + "?" + b;
+		getMovies(url);
+	}
+}
